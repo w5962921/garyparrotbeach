c9cec306da7672f54f7d25c1f905446f43f71cca
w5962921 <w5962921@gmail.com>
2026-05-11 18:04:35 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@/components'] = path.resolve(__dirname, 'components');
-    return config;
+  experimental: {
+    turbopack: {
+      aliases: {
+        '@/components/*': ['./components/*'],
+      },
+    },
   },
 };
 
