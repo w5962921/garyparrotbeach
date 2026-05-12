da449db4cbe73eae7163f4f3c30a5044ccbb6862
w5962921 <w5962921@gmail.com>
2026-05-11 17:55:11 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // This forces Next.js to realize your files are sitting right in the root!
-  experimental: {
-    appDir: false
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
