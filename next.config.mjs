0db2c3018c5653bc55ecefef2887bea2ab78049d
w5962921 <w5962921@gmail.com>
2026-05-11 18:07:52 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbopack: {
-      aliases: {
-        '@/components/*': ['./components/*'],
-      },
-    },
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
